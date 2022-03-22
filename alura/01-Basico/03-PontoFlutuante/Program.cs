2637ad3955c7caa34d30d237beeedf572dfae26f
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-03-21 23:17:52 -0300

    add float point

---
+++ alura/01-Basico/03-PontoFlutuante/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03_PontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 1975.50;
+
+            Console.WriteLine($"O meu salário é {salario}");
+        }
+    }
+}
