8655dc3a3263b237927f43f698527f91ffcbfbd3
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-03-21 23:18:15 -0300

    add conversions and numeric types

---
+++ alura/01-Basico/04-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 1975.50;
+            // Casting
+            int valor = (int)salario;
+            System.Console.WriteLine($"O salário em inteiro é {valor}");
+
+            // long - 64 bits
+            long idadeDoUniverso = 14000000000;
+            System.Console.WriteLine($"A idade do universo é {idadeDoUniverso} de anos.");
+
+            // short
+            short qtdeProdutos = 150;           
+            System.Console.WriteLine($"Tem {qtdeProdutos} produtos no estoque!");
+        }
+    }
+}
