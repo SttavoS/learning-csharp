99c3d6316110cd8b18b77f0f1101b7d7c50e8d63
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-03-21 22:11:28 -0300

    add integer vars

---
+++ alura/01-Basico/02-VariaveisInt/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02_Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 23;
+
+            Console.WriteLine($"Minha idade é {idade}!");            
+        }        
+    }
+}
