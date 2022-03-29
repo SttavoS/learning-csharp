02595df60bb9b62539a9af2bfa7e0a2e2d0a0971
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-03-28 23:10:12 -0300

    add 05

---
+++ alura/01-Basico/05-CharsEStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char letra = 'a';
+        System.Console.WriteLine(letra);
+
+        char valorDaTabelaAscii = (char)65;
+        Console.WriteLine(valorDaTabelaAscii);
+
+        valorDaTabelaAscii = (char) (valorDaTabelaAscii + 1);
+        Console.WriteLine(valorDaTabelaAscii);
+    }
+}
