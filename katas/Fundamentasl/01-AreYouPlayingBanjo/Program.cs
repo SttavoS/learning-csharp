01292a25f2bc9f50945efffa5b39d7a4fcbbaa3f
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-03-29 22:53:03 -0300

    add first kata

---
+++ katas/Fundamentasl/01-AreYouPlayingBanjo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(AreYouPlayingBanjo("Martin"));
+        Console.WriteLine(AreYouPlayingBanjo("Rikke"));
+        Console.WriteLine(AreYouPlayingBanjo("bravo"));
+        Console.WriteLine(AreYouPlayingBanjo("rolf"));
+    }
+
+    public static string AreYouPlayingBanjo(string name)
+    {
+        char firstLetter = name.First<char>();
+        if (firstLetter.Equals('R') || firstLetter.Equals('r'))
+        {
+            return $"{name} plays banjo";
+        }
+        else
+        {
+            return $"{name} does not play banjo";
+        }
+    }
+}
---
+++ katas/Fundamentasl/01-AreYouPlayingBanjo/README.md
@@ -0,0 +1,11 @@
+### **8 Kyu - Are You Playing Banjo?**
+
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+```c#
+name + " plays banjo" 
+name + " does not play banjo"
+```
