7c6b80c453fbcc4f0258ad4128a2fe3197bb8689
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-03-29 22:15:15 -0300

    add condicionais

---
+++ alura/01-Basico/06-Condicionais/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idade = 15;
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("É maior de idade!");
+        }
+        else
+        {
+            Console.WriteLine("É menor de idade!");
+        }
+    }
+}
