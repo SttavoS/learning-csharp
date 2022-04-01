8abe32694253386c18d8528952166b0703f5a363
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-03-31 22:24:59 -0300

    add 08 for

---
+++ alura/01-Basico/08-For/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double valorInvestido = 1500;
+        double fatorRendimento = 1.0036;
+
+        for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+        {
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= fatorRendimento;
+                Console.WriteLine($"Ano: {contadorAno} | Mes: {contadorMes} | Valor R$ {valorInvestido.ToString("0.00")}");
+            }
+            fatorRendimento += 0.0010;
+        }
+    }
+}
