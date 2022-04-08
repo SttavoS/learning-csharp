943725772e5413cd541635f92afc57422aba3716
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-04-07 21:19:10 -0300

    inicio

---
+++ alura/02-POO/ByteBank/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaCorrente conta = new ContaCorrente();
+        conta.titular = "Gustavo";
+        conta.agencia = 8639;
+        conta.numero = 123456;
+        conta.saldo = 100;
+
+        Console.WriteLine($"O titular da conta é {conta.titular}");
+    }
+}
