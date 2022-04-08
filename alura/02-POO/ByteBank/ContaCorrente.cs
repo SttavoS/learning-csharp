310b7df59a0ad5ca7d247177df77c49ba7266310
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-04-07 22:08:57 -0300

    added methods

--- alura/02-POO/ByteBank/ContaCorrente.cs
+++ alura/02-POO/ByteBank/ContaCorrente.cs
@@ -4,4 +4,32 @@ public class ContaCorrente
     public int agencia;
     public int numero;
     public double saldo = 100;
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;        
+    }
+
+    public bool Sacar(double valor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+
+        this.saldo -= valor;
+        return true;
+    }
+
+    public bool Tranferir(double valor, ContaCorrente contaDestino)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+
+        this.saldo -= valor;
+        contaDestino.Depositar(valor);
+        return true;
+    }
 }
--- alura/02-POO/ByteBank/Program.cs
+++ alura/02-POO/ByteBank/Program.cs
@@ -4,12 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        ContaCorrente conta = new ContaCorrente();
-        conta.titular = "Gustavo";
-        conta.agencia = 8639;
-        conta.numero = 123456;
-        conta.saldo = 100;
+        ContaCorrente conta1 = new ContaCorrente();
+        conta1.titular = "Gustavo";
+        conta1.agencia = 8639;
+        conta1.numero = 123456;
+        conta1.saldo = 100;
 
-        Console.WriteLine($"O titular da conta é {conta.titular}");
+        Console.WriteLine($"O titular da conta 1 é {conta1.titular}");
+
+        conta1.Depositar(500);
+        bool resultadoSaque = conta1.Sacar(200);
+        Console.WriteLine($"O resultado do saque foi {resultadoSaque}");
+
+        ContaCorrente conta2 = new ContaCorrente();
+        conta1.Tranferir(150, conta2);
+        Console.WriteLine($"O saldo da conta 1 é de R$ {conta1.saldo}");
+        Console.WriteLine($"O saldo da conta 2 é de R$ {conta2.saldo}");
     }
 }
