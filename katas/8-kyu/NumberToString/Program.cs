92bffb13d8e66185c64c81860a6729ad3d997507
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-04-13 22:08:32 -0300

    add kata NumberToString

---
+++ katas/8-kyu/NumberToString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(NumberToString(67));
+        Console.WriteLine(NumberToString(79585));
+        Console.WriteLine(NumberToString(1 + 2));
+        Console.WriteLine(NumberToString(1 - 2));
+    }
+
+    public static string NumberToString(int number) 
+    {
+        return number.ToString();
+    }
+}
