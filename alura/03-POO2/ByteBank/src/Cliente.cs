a623d50a5377ec2aa571ba74a69a37f960b41e60
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-04-22 22:13:01 -0300

    converted to file-scoped namespaces

--- alura/03-POO2/ByteBank/src/Cliente.cs
+++ alura/03-POO2/ByteBank/src/Cliente.cs
@@ -1,16 +1,15 @@
-namespace ByteBank
-{
-    public class Cliente
-    {
-        public string Nome { get; private set; }
-        public string Cpf { get; private set; }
-        public string Profissao { get; private set; }
+namespace ByteBank;
 
-        public Cliente(string nome, string cpf, string profissao)
-        {
-            this.Nome = nome;
-            this.Cpf = cpf;
-            this.Profissao = profissao;
-        }
-    }    
-}
+public class Cliente
+{
+    public string Nome { get; private set; }
+    public string Cpf { get; private set; }
+    public string Profissao { get; private set; }
+
+    public Cliente(string nome, string cpf, string profissao)
+    {
+        this.Nome = nome;
+        this.Cpf = cpf;
+        this.Profissao = profissao;
+    }
+}    
--- alura/03-POO2/ByteBank/src/Program.cs
+++ alura/03-POO2/ByteBank/src/Program.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 
-namespace ByteBank
+namespace ByteBank;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Cliente gustavo = new Cliente("Gustavo Schneider", "123.456.789-10", "Desenvolvedor");
-            ContaCorrente conta1 = new ContaCorrente(gustavo, 863);
+        Cliente gustavo = new Cliente("Gustavo Schneider", "123.456.789-10", "Desenvolvedor");
+        ContaCorrente conta1 = new ContaCorrente(gustavo, 863);
 
-            Console.WriteLine($"O titular da conta 1 é {conta1.Titular.Nome}");
-            Console.WriteLine($"A agência da conta é {conta1.Agencia} e o número {conta1.Numero}");
-            Console.WriteLine($"Com saldo de {conta1.Saldo}");
+        Console.WriteLine($"O titular da conta 1 é {conta1.Titular.Nome}");
+        Console.WriteLine($"A agência da conta é {conta1.Agencia} e o número {conta1.Numero}");
+        Console.WriteLine($"Com saldo de {conta1.Saldo}");
 
-            conta1.Depositar(500);
-            Console.WriteLine($"Saldo após o deposito é de {conta1.Saldo}");
-            conta1.Sacar(200);
-            Console.WriteLine($"Saldo após o saque é de {conta1.Saldo}");
-        }
+        conta1.Depositar(500);
+        Console.WriteLine($"Saldo após o deposito é de {conta1.Saldo}");
+        conta1.Sacar(200);
+        Console.WriteLine($"Saldo após o saque é de {conta1.Saldo}");
     }
 }
