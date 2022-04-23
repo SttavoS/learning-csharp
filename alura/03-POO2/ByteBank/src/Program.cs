e9ae41da5f6084d08168690b4db778ca608ee6db
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-04-22 22:13:54 -0300

    converted to use var keyword

--- alura/03-POO2/ByteBank/src/Program.cs
+++ alura/03-POO2/ByteBank/src/Program.cs
@@ -6,8 +6,8 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Cliente gustavo = new Cliente("Gustavo Schneider", "123.456.789-10", "Desenvolvedor");
-        ContaCorrente conta1 = new ContaCorrente(gustavo, 863);
+        var gustavo = new Cliente("Gustavo Schneider", "123.456.789-10", "Desenvolvedor");
+        var conta1 = new ContaCorrente(gustavo, 863);
 
         Console.WriteLine($"O titular da conta 1 é {conta1.Titular.Nome}");
         Console.WriteLine($"A agência da conta é {conta1.Agencia} e o número {conta1.Numero}");
