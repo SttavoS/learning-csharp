ceb58d331a1aa4aa26e4ec6bb6f957717807c576
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-05-09 21:06:58 -0300

    add division by zero example

---
+++ alura/04-Excecoes/DivisionByZero/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int number = 10;
+            int zero = 0;
+
+            int divisao = number / zero;
+
+            Console.WriteLine(divisao);
+        }
+        catch (DivideByZeroException)
+        {
+
+            Console.WriteLine("Tentativa de divisão por zero interceptada.");
+        }
+    }
+}
