f37ef7a31ec93118b9a9c084842e238c20eb5539
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-05-09 22:56:00 -0300

    kata: add SpacesRemover

---
+++ katas/8-kyu/SpacesRemover/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(NoSpaces("8 j 8   mBliB8g  imjB8B8  jl  "));
+        Console.WriteLine(NoSpaces("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
+        Console.WriteLine(NoSpaces("8aaaaa dddd r     "));
+    }
+
+    private static string NoSpaces(string input)
+        => input.Trim().Replace(" ", "");
+}
