46b1ea48a62b928fb2bdf010e42bd21859cc9476
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-05-31 21:09:07 -0300

    add convert string to number

---
+++ katas/8-kyu/StringToNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(SringToNumber("1234"));
+        Console.WriteLine(SringToNumber("605"));
+        Console.WriteLine(SringToNumber("1405"));
+        Console.WriteLine(SringToNumber("-7"));
+    }
+
+    private static int SringToNumber(string input)
+        => Int32.Parse(input);
+}
