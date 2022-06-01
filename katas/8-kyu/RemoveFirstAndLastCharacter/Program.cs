1660de09c7994538c39c3896f811df69bbbcf02e
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2022-05-31 21:27:58 -0300

    add remove first and last character

---
+++ katas/8-kyu/RemoveFirstAndLastCharacter/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RemoveChars("eloquent"));
+        Console.WriteLine(RemoveChars("country"));
+        Console.WriteLine(RemoveChars("person"));
+        Console.WriteLine(RemoveChars("place"));
+        Console.WriteLine(RemoveChars("ok"));
+    }
+
+    private static string RemoveChars(string input)
+    {
+        var lastChar = input.Length - 1;
+        return input.Remove(lastChar, 1).Remove(0, 1);
+    }
+}
