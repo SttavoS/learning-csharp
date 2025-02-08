d65d228a2600b91a88ff2e4bd11c7338aad835fa
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2025-02-08 17:54:52 -0300

    08-02-2025: resolve a new kata

---
+++ katas/8-kyu/HowManyLightsaberDoYouOwn/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine(HowManyLightsabersDoYouOwn("Adam"));
+    Console.WriteLine(HowManyLightsabersDoYouOwn("Zach"));
+  }
+
+  public static int HowManyLightsabersDoYouOwn(string name)
+  {
+    return name.Equals("Zach") ? 18 : 0;
+  }
+}
